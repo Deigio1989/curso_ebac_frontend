1c1ad99e8955a2a726620331f61ef74b96ca557b
Deigio1989 <130110285+Deigio1989@users.noreply.github.com>
2024-03-12 13:31:46 -0300

    commit 1

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle:'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+
+}
+
+function funcaoPadrao(callback){
+    compilaSass();
+    callback();
+}
+
+exports.default = compilaSass;
+exports.sass = compilaSass;

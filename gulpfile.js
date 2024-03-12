1ac24adec91a4b4262c4fa65a6237dec58997afb
Deigio1989 <130110285+Deigio1989@users.noreply.github.com>
2024-03-12 14:32:00 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'))
-const sourcemaps = require('gulp-sourcemaps')
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+
+function comprimeImagens(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
 
 function compilaSass(){
     return gulp.src('./source/styles/main.scss')
@@ -9,14 +27,13 @@ function compilaSass(){
             outputStyle:'compressed'
         }))
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./build/styles'))
-
+        .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao(callback){
-    compilaSass();
-    callback();
-}
 
-exports.default = compilaSass;
-exports.sass = compilaSass;
+exports.default = function(){
+    gulp.watch('./source/styles/*.scss',{ignoreInitial:false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js',{ignoreInitial:false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*',{ignoreInitial:false}, gulp.series(comprimeImagens));
+    
+}
